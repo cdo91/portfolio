c68cf23c792adf7b3bf0858057df38c2a05ab325
Christian Olofsson <ch.olofsson@protonmail.com>
2023-02-19 14:22:06 +0100

    media query changes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,21 +7,21 @@ import HeaderSocials from './HeaderSocials'
 const Header = () => {
   
   return (
-    <header>
+    <>
+      <section>
+      <header>
       <div className="container header__container">
         <h5>Mitt namn är</h5>
         <h1>Christian Olofsson</h1>
         <h5 className="text-light">Front-End Utvecklare</h5>
         <CTA />
         <HeaderSocials />
-
-        <div className="me">
-          <img src={ME} alt="" />
-        </div>
-
+        <img className="me" src={ME} alt="" />
         <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
+      </section>
+    </>
   )
 }
 
