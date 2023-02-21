e78e4c188fac475417443fc765e7671007c18aa4
Christian Olofsson <ch.olofsson@protonmail.com>
2023-02-21 14:58:51 +0100

    first commit

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -17,28 +17,28 @@ const Services = () => {
             </div>
             <ul className='service__list'>
               <li>
-                <BiCheck style={{width: '5rem'}} className='service__list-icon' />
-                <p>Användbarhetsanalys och testning - Uföra användbarhetsanalyser samt tester för att bedöma webbplatser och appar från användarperspektiv och identifiera eventuella användarproblem och förbättringsområden.</p>
+                <BiCheck style={{width: '4.6rem'}} className='service__list-icon' />
+                <p>Användbarhetsanalys och testning - Uföra analyser samt tester för att bedöma webbplatser och appar från ett användarperspektiv. Identifiera eventuella problem och förbättringsområden.</p>
               </li>
               <li>
-                <BiCheck style={{width: '4.5rem'}} className='service__list-icon' />
-                <p>Gränssnittsdesign - Utforma användarvänliga gränssnitt som underlättar navigering och hjälper användare att uppnå sina mål på ett effektivt sätt, med ett fokus på användarupplevelsen.</p>
+                <BiCheck style={{width: '3.5rem'}} className='service__list-icon' />
+                <p>Gränssnittsdesign - Utforma användarvänliga gränssnitt som underlättar navigering och hjälper besökaren att uppnå sina mål på ett effektivt sätt.</p>
               </li>
               <li>
                 <BiCheck style={{width: '4rem'}} className='service__list-icon' />
                 <p>Interaktionsdesign - Utveckla interaktiva och responsiva användargränssnitt som möjliggör naturliga samt intuitiva interaktioner mellan användare och gränssnittet.</p>
               </li>
               <li>
-                <BiCheck style={{width: '5rem'}} className='service__list-icon' />
-                <p>Visuell design - Skapa estetiskt tilltalande och enhetliga designspråk som överensstämmer med varumärkesriktlinjer och kommunicerar en enhetlig känsla och identitet för webbplatsen eller appen.</p>
+                <BiCheck style={{width: '4.3rem'}} className='service__list-icon' />
+                <p>Visuell design - Skapa estetiskt tilltalande och enhetliga designspråk som överensstämmer med riktlinjer och framhäver en konsekvent identitet för webbplatsen eller appen.</p>
+              </li>
+              <li>
+                <BiCheck style={{width: '4.8rem'}} className='service__list-icon' />
+                <p>Användarcentrerad design - Integrera samlade principer och metoder i designprocessen för att skapa en effektiv samt användarvänlig utformning som möter besökarens behov och förväntningar.</p>
               </li>
               <li>
                 <BiCheck style={{width: '5rem'}} className='service__list-icon' />
-                <p>Användarcentrerad design - Integrera användarcentrerade principer och metoder i hela designprocessen för att skapa en effektiv samt användarvänlig design som möter användarnas behov och förväntningar.</p>
-              </li>
-              <li>
-                <BiCheck style={{width: '5rem'}} className='service__list-icon' />
-                <p>Prototyper och testning - Skapa prototyper och interaktiva mockups för att testa användbarhet samt funktionalitet i tidiga skeden av designprocessen, sedan använda feedbacken för att förbättra designen innan den implementeras.</p>
+                <p>Prototyper och testning - Skapa interaktiva skisser för att testa användbarhet samt funktionalitet i tidigare skeden av designprocessen. Feedback kan sedan användas för att förbättra designen innan den tillämpas.</p>
               </li>
             </ul>
           </article>
@@ -49,31 +49,31 @@ const Services = () => {
             <ul className='service__list'>
               <li>
                 <BiCheck style={{width: '3rem'}} className='service__list-icon' />
-                <p>Webbdesign - Skapa och implementera snygga samt funktionella designkoncept som passar kundens behov och varumärke.</p>
+                <p>Webbdesign - Skapa och applicera snygga samt funktionella designkoncept som passar kundens behov och varumärke.</p>
               </li>
               <li>
-                <BiCheck style={{width: '2.5rem'}} className='service__list-icon' />
-                <p>Anpassning av befintliga teman - Anpassa befintliga webbteman för att bättre matcha kundens krav och behov.</p>
+                <BiCheck style={{width: '2.7rem'}} className='service__list-icon' />
+                <p>Anpassning av befintliga teman - Anpassa redan existerande webbteman för att bättre matcha kundens krav.</p>
               </li>
               <li>
-                <BiCheck style={{width: '3rem'}} className='service__list-icon' />
-                <p>Webbplatsutveckling - Bygga responsiva och användarvänliga webbplatser som laddar snabbt samt fungerar på alla enheter.</p>
+                <BiCheck style={{width: '3.2rem'}} className='service__list-icon' />
+                <p>Webbapplikationsutveckling - Bygga responsiva och användarvänliga webbplatser som laddar snabbt samt fungerar på alla enheter.</p>
               </li>
               <li>
-                <BiCheck style={{width: '3rem'}} className='service__list-icon' />
-                <p>Animering - Skapa dynamiska och engagerande webbsidor med hjälp av CSS-animationer samt JavaScript-effekter.</p>
+                <BiCheck style={{width: '2.8rem'}} className='service__list-icon' />
+                <p>Animering - Skapa dynamiska och engagerande webbplatser med hjälp av CSS-animationer samt JavaScript-effekter.</p>
               </li>
               <li>
-                <BiCheck style={{width: '4rem'}} className='service__list-icon' />
-                <p>Optimering för SEO - Optimera webbsidor för sökmotoroptimering (SEO) och därigenom hjälpa kunder att nå högre placeringar på sökmotorresultatsidor (SERP).</p>
+                <BiCheck style={{width: '3.5rem'}} className='service__list-icon' />
+                <p>Optimering av prestanda - Optimera webbplatsens laddningstid och prestanda genom att justera bilder, komprimera kod och genomföra cachning.</p>
               </li>
               <li>
-                <BiCheck style={{width: '5rem'}} className='service__list-icon' />
-              <p>Webbsidetestning och buggfixning - Genomföra omfattande testning av webbsidor för att hitta buggar och andra problem, sedan lösa dessa problem för att säkerställa att webbsidan fungerar korrekt på alla enheter.</p>
+                <BiCheck style={{width: '4.3rem'}} className='service__list-icon' />
+              <p>Webbapplikationstestning - Genomföra omfattande testning av webbapplikationer för att hitta buggar och andra problem. Säkerställa att webbplatsen fungerar korrekt på alla enheter.</p>
               </li>
               <li>
-                <BiCheck style={{width: '5rem'}} className='service__list-icon' />
-                <p>Underhåll av befintliga webbplatser - Uppdatera, underhålla och förbättra befintliga webbplatser för att säkerställa att de fungerar korrekt samt att de möter de senaste standarderna och kraven för webbutveckling.</p>
+                <BiCheck style={{width: '4.4rem'}} className='service__list-icon' />
+                <p>Underhåll av befintliga webbapplikationer - Uppdatera, underhålla och förbättra befintliga webbplatser för att garantera att de möter den senaste normen och kraven för webbutveckling.</p>
               </li>
             </ul>
           </article>
